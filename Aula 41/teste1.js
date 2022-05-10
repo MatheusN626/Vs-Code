93a9bf5f911cdb141b49c395f0afe54bef3db54a
MatheusN626 <themastermack@hotmail.com>
2022-05-10 14:34:34 -0300

    Aula

---
+++ Aula 41/teste1.js
@@ -0,0 +1,15 @@
+//declarando um objeto na notação JSON
+var pessoa = {
+    //propriedade : valor
+    "nome" : "Cassiano",
+    "idade" : 32
+}
+
+//obtendo dados do JSON
+
+console.log("Dados da pessoa: ", pessoa)
+
+//obtendo uma proriedade do objeto pessoa
+var idade = pessoa.idade;
+
+console.log("Idade da pessoa:", idade)
